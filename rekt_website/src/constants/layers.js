b6b580be00efb402e47198b8b2a810e86fa6ca00
t-phoenix <abhijaipur2011@gmail.com>
2025-08-19 01:35:27 +0530

    Revert "adding revamped pfp design"
    This reverts commit 864649ecc845a0180a43aee0f85c6d5c3adfbb83.

--- rekt_website/src/constants/layers.js
+++ rekt_website/src/constants/layers.js
@@ -12,9 +12,9 @@ import hoodie1 from "../creatives/pfp/hoodie/hoodie1.png";
 import hoodie2 from "../creatives/pfp/hoodie/hoodie2.png";
 import hoodie3 from "../creatives/pfp/hoodie/hoodie3.png";
 import hoodie4 from "../creatives/pfp/hoodie/hoodie4.png";
-// import hoodie5 from "../creatives/pfp/hoodie/hoodie5.png";
-// import hoodie6 from "../creatives/pfp/hoodie/hoodie6.png";
-// import hoodie7 from "../creatives/pfp/hoodie/hoodie7.png";
+import hoodie5 from "../creatives/pfp/hoodie/hoodie5.png";
+import hoodie6 from "../creatives/pfp/hoodie/hoodie6.png";
+import hoodie7 from "../creatives/pfp/hoodie/hoodie7.png";
 
 import pant1 from "../creatives/pfp/pants/pant1.png";
 import pant2 from "../creatives/pfp/pants/pant2.png";
@@ -39,12 +39,7 @@ import face3 from "../creatives/pfp/faces/face3.png"
 import face4 from "../creatives/pfp/faces/face4.png"
 import face5 from "../creatives/pfp/faces/face5.png"
 import face6 from "../creatives/pfp/faces/face6.png"
-// import face7 from "../creatives/pfp/faces/face7.png"
-
-import jewellery1 from "../creatives/pfp/jewellery/jewellery1.png";
-import jewellery2 from "../creatives/pfp/jewellery/jewellery2.png";
-import jewellery3 from "../creatives/pfp/jewellery/jewellery3.png";
-import jewellery4 from "../creatives/pfp/jewellery/jewellery4.png";
+import face7 from "../creatives/pfp/faces/face7.png"
 
 import btc from "../creatives/pfp/coins/btc.png";
 import doge from "../creatives/pfp/coins/doge.png";
@@ -58,23 +53,21 @@ import usdc from "../creatives/pfp/coins/usdc.png";
 
 export const layers = [
     [BG1, BG2, BG3, BG4, BG5, BG6, BG7], 
-    [hoodie1, hoodie2, hoodie3, hoodie4], 
+    [hoodie1, hoodie2, hoodie3, hoodie4, hoodie5, hoodie6, hoodie7], 
     [pant1, pant2, pant3, pant4],
     [shoe1, shoe2, shoe3, shoe4, shoe5, shoe6],
     [skin1, skin2, skin3, skin4],
-    [face1, face2, face3, face4, face5, face6],
-    [jewellery1, jewellery2, jewellery3, jewellery4],
+    [face1, face2, face3, face4, face5, face6, face7],
     [btc, doge, eth, pepe, pump, rekt, shiba, usdc]
 ]
 
 export const layerNames = [
     ["BG1", "BG2", "BG3", "BG4", "BG5", "BG6", "BG7"], 
-    ["Hoodie1", "Hoodie2", "Hoodie3", "Hoodie4"], 
+    ["Hoodie1", "Hoodie2", "Hoodie3", "Hoodie4", "Hoodie5", "Hoodie6", "Hoodie7"], 
     ["Pant1", "Pant2", "Pant3", "Pant4"],
     ["Shoe1", "Shoe2", "Shoe3", "Shoe4", "Shoe5", "Shoe6"],
     ["Skin1", "Skin2", "Skin3", "Skin4"],
-    ["Face1", "Face2", "Face3", "Face4", "Face5", "Face6"],
-    ["Jewellery1", "Jewellery2", "Jewellery3", "Jewellery4"],
+    ["Face1", "Face2", "Face3", "Face4", "Face5", "Face6", "Face7"],
     ["btc", "doge", "eth", "pepe", "pump" , "rekt", "shiba", "usdc"]
 ]
 
@@ -87,6 +80,5 @@ export const items = [
     "Shoes",
     "Skin",
     "Face",
-    "Jewellery",
     "Coin",
   ];
